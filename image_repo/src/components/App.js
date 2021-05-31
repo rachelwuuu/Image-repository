4dab2f965b824c91eb426362990c4dfe90c096e9
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-05-30 22:22:19 -0400

    added html to the Signup.js, added <Signup/> function to App.js

--- image_repo/src/components/App.js
+++ image_repo/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
-
+import Signup from "./Signup";
 function App() {
-  return "Hello World"
+  return <Signup />
 }
 
 export default App;
---
+++ image_repo/src/components/Signup.js
@@ -0,0 +1,28 @@
+import React, {useRef} from 'react'
+import {Form, Button, Card} from 'react-bootstrap'
+
+export default function Signup() {
+    const emailRef=useRef();
+    const passwordRef=useRef();
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2>Sign Up</h2>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required/>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required/>
+                        </Form.Group>
+                        <Button type="submit">Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div>yea</div>
+        </>
+    )
+}
