2b5943970d9f8845e9ad094738af0f6cf5482c90
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-06-02 23:47:35 -0400

    password do not match

--- image_repo/src/components/Signup.js
+++ image_repo/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from 'react'
-import {Form, Button, Card} from 'react-bootstrap'
+import {Form, Button, Card, Alert} from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
 import {Link, useHistory} from "react-router-dom"
 export default function Signup() {
@@ -17,6 +17,7 @@ export default function Signup() {
         try {
             setError("")
             setLoading(true)
+            await Signup(emailRef.current.value, passwordRef.current.value)
             history.push("/")//push an entry in the session's history stack
         }catch{
             setError("Failed to create account.")
@@ -29,7 +30,8 @@ export default function Signup() {
             <Card>
                 <Card.Body>
                     <h2 className="text-center">Sign Up</h2>
-                    <Form>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required/>
@@ -42,7 +44,7 @@ export default function Signup() {
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control type="password" ref={passwordConfirmRef} required/>
                         </Form.Group>
-                        <Button type="submit">Sign Up</Button>
+                        <Button disabled={loading} type="submit">Sign Up</Button>
                     </Form>
                 </Card.Body>
             </Card>
