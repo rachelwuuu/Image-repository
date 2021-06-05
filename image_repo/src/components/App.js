cdfe36e04ff0975fd4a09d992c85488087fd6858
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-06-04 23:12:54 -0400

    added Dashboard and Login component

--- image_repo/src/components/App.js
+++ image_repo/src/components/App.js
@@ -2,17 +2,23 @@ import React from "react"
 import Signup from "./Signup";
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Dashboard from 'Dashboard.js'
+import Login from 'Login.js'
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center"
+    <Container className="d-flex align-items-center justify-content-center"
       style={{minHeight:"100vh"}}
       >
-        <div style={{maxWidth:"600px"}}><Signup /></div>
-        
-      </Container>
-    </AuthProvider>
-    
+      <div style={{maxWidth:"600px"}}><Signup /></div>
+      <AuthProvider>
+        <Switch>
+        <Route path="/" component={Dashboard}/>
+          <Route path="/signup" component={Signup}/>
+          <Route path="/login" component={Login}/>
+        </Switch>
+      </AuthProvider>
+    </Container>
       
   )
 }
