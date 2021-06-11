0e6ccb678ed2c1ca70b1f6cd424fd4c54330fafe
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-06-10 23:48:56 -0400

    edited image restriction in FormSubmit.js, and added a hook useStorage.js

--- image_repo/src/components/FormSubmit.js
+++ image_repo/src/components/FormSubmit.js
@@ -3,20 +3,20 @@ import React, {useState} from 'react'
 const FormSubmit=()=> {
     const [file, setFile]= useState(null);
     const [error, setError]= useState(null);
-    const types=['image/png','image/peg']
     const changeHandler = (e) => {
         let selected=e.target.files[0];
-        if(selected&&types.includes(selected.type)){
+        if(selected){
             setFile(selected);
             setError("");
         }else{
             setFile(null);
-            setError("You can't submit files other than png and peg.")
+            setError("You can't submit files other than jpg, gif, png and peg.")
         }
     }
     return (
         <form>
-            <input type="file" onChange={changeHandler}/>
+            <input type="file" onChange={changeHandler} accept=".png, .jpeg, .jpg, .gif"/>
+            {/*This works better than having it in the js code(const type=[image/png,...]; if(selected&&types.include(selected.type)){...}), as it automatically limits the file types, and the js code doesn't work for .jpg*/}
             <div className="output">
                 {error&&<div className="error">{error}</div>}
                 {file&&<div>{file.name}</div>}
